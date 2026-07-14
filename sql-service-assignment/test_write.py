afc9fbdcfa9e37ce6e939cc6aafed4a6355681f3
Akshansh-Sharma <98akshansh@gmail.com>
2026-07-14 21:45:22 +0530

    Delete sql-service-assignment/test_write.py

--- sql-service-assignment/test_write.py
+++
@@ -1,7 +0,0 @@
-import os
-try:
-    with open("/home/akshansh/sandbox/maarg/runtime/component/sql-service-assignment/log.txt", "w") as f:
-        f.write("Workspace is writable!\n")
-    print("Write successful!")
-except Exception as e:
-    print("Write failed:", e)
